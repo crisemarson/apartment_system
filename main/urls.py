a1a51b2b42490dbec05715e4b01627f8e03a7a24
Shaira <mallari.shaira31@gmail.com>
2024-07-09 14:33:28 -0500

    creating and viewing post

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('sign-up', views.sign_up, name='sign_up'),
-   
+    path('create-post', views.create_post, name='create_post'),
 
     
 ]
